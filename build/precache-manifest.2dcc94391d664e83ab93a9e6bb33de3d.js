c0971d467a4e481e96984c03395dc156070e8bad
HappyDev <sabahat.ullah@aol.com>
2020-06-29 18:06:31 +0100

    adding build

---
+++ build/precache-manifest.2dcc94391d664e83ab93a9e6bb33de3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3c3e412be14eb7b92957faae61068ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9affef20e19fa0b02161",
+    "url": "/static/css/main.3bea93b1.chunk.css"
+  },
+  {
+    "revision": "cbbde6da348a01c77c79",
+    "url": "/static/js/2.8f0b0bea.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.8f0b0bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9affef20e19fa0b02161",
+    "url": "/static/js/main.0495d548.chunk.js"
+  },
+  {
+    "revision": "d3be38e7f455fb7c073c",
+    "url": "/static/js/runtime-main.25917a94.js"
+  }
+]);
