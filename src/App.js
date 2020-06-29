6bd4141d15c68ea48fa8587a1a69d4d5f111b9e1
sabahatullah <sabahat.ullah@aol.com>
2020-06-29 12:47:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Cards, Chart, CountryPicker} from './components'
+
+import styles from './App.module.css';
+import { fetchData } from './api';
+
+// TUTORIAL FOR IMPORT/EXPORT https://www.youtube.com/watch?v=s9kNndJLOjg
+
+class App extends React.Component{
+    state = {
+        data: {},
+        country: '',
+    }
+    // console.log(data)
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        // console.log(fetchedData);
+
+        this.setState({data:fetchedData})
+
+    }
+    handleCountryChange = async(country) => {
+        const fetchedData = await fetchData(country)
+        this.setState({data:fetchedData, country:country})
+        console.log(country);
+        
+    }
+    render(){
+        const {data, country} = this.state;
+        return(
+            <div className={styles.container}>
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country}></Chart>
+                
+            </div>
+
+        )
+    }
+
+
 }
+
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async(country) => {
+    let changeableURL = url;
+
+    if(country){
+        changeableURL = `${url}/countries/${country}`;
+    }
+
+    try{
+        
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(changeableURL);
+        // const data = await axios.get(url);
+        
+        return { confirmed, recovered, deaths, lastUpdate }
+        // return {data};
+        
+    }catch(error){
+
+    }
+}
+
+export const fetchDailyData = async() => {
+    try{
+        const {data} = await axios.get( `${url}/daily`);
+        // console.log(data)
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed:dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }))
+        return modifiedData
+    }catch(error){
+
+    }
+}
+
+export const fetchCountries = async() => {
+    try{
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+        // console.log(countries);
+
+        return countries.map((country)=>country.name)
+    }catch(error){
+
+    }
+
+}
+
